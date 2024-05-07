4b0dac0e53019132bb2b91bc2fb770f94620108b
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-05-07 00:10:13 -0300

    :recycle: refactor: readCoffees JS

---
+++ src/js/readCoffees.js
@@ -0,0 +1,61 @@
+import { requestApi } from "./requestApi.js";
+
+export async function readCoffees(data) {
+  data.forEach((coffee) => {
+    const menu = document.querySelector(".products");
+
+    const coffeeColumn = document.createElement("div");
+    coffeeColumn.classList.add("col");
+
+    const coffeeCard = document.createElement("div");
+    coffeeCard.classList.add("card", "h-100");
+
+    coffeeCard.innerHTML = `
+    <img src='${
+      coffee.img
+    }' class="card-img-top img-fluid" alt="Imagem do café ${coffee.id}">
+    <div class="card-body">
+      <h5 class="card-title text-center" id="coffe-title">${coffee.id} - R$${
+      coffee.features.price
+    }</h5>
+
+      <div class="container-fluid text-center">
+        <div class="row">
+          <div class="col-6">
+            <strong>Type</strong>
+            <p>${coffee.features.type.toUpperCase()}</p>
+          </div>
+          <div class="col-6">
+            <strong>Organic</strong>
+            <p>${coffee.features.organic.toUpperCase()}</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-6">
+            <strong>Origin</strong>
+            <p>${coffee.features.origin.toUpperCase()}</p>
+          </div>
+          <div class="col-6">
+            <strong>Roast</strong>
+            <p>${coffee.features.roast.toUpperCase()}</p>
+          </div>
+        </div>
+      </div>
+        
+      <div class="card-footer">
+        <button type="button" class="btn btn-success w-100">Add to cart</button>
+      </div>
+        `;
+
+    menu.appendChild(coffeeColumn);
+    coffeeColumn.appendChild(coffeeCard);
+  });
+}
+
+requestApi("./src/json/shopData.json")
+  .then((data) => {
+    readCoffees(data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+});
